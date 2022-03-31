7299a9e79baa93dec230a6ba09b4febfb05c2ba5
Lowri Williams <lowri.a.williams@gmail.com>
2022-03-31 19:15:00 +0100

    31/03

---
+++ assets/js/symptom_checker_graph.js
@@ -0,0 +1,3 @@
+var illness = ['Breathing difficulties/wheezing', 'Chest pain/back pain', 'Cold and flu', 'Cough', 'Generally unwell'];
+var mar19 = [0, 0, 340, 1043, 2380];
+var mar20 = [2349, 3479, 6368, 21965, 16414];
